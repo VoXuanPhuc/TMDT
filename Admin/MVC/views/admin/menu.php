2ae46fdb54673362176379a352f9f510da4ead9c
Thuthuy1311 <72400802+Thuthuy1311@users.noreply.github.com>
2022-06-07 21:47:38 +0700

    edit

--- Admin/MVC/views/admin/menu.php
+++ Admin/MVC/views/admin/menu.php
@@ -1,39 +1,39 @@
   <!-- Navbar-->
   <header class="app-header">
-    <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
-    <!-- Navbar Right Menu-->
-    <ul class="app-nav">
+      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      <!-- Navbar Right Menu-->
+      <ul class="app-nav">
 
 
-      <!-- User Menu-->
-      <li><a class="app-nav__item" href="../"><i class='bx bx-log-out bx-rotate-180'></i> </a>
+          <!-- User Menu-->
+          <li><a class="app-nav__item" href="../"><i class='bx bx-log-out bx-rotate-180'></i> </a>
 
-      </li>
-    </ul>
+          </li>
+      </ul>
   </header>
   <!-- Sidebar menu-->
   <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
   <aside class="app-sidebar">
-    <div class="app-sidebar__user">
-      <img class="app-sidebar__user-avatar" src="https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg" width="50px" alt="User Image">!-
-      <div>
-        <p class="app-sidebar__user-name"><b>Admin</b></p>
-        <p class="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
+      <div class="app-sidebar__user">
+          <img class="app-sidebar__user-avatar" src="https://static2.yan.vn/YanNews/2167221/202102/facebook-cap-nhat-avatar-doi-voi-tai-khoan-khong-su-dung-anh-dai-dien-e4abd14d.jpg" width="50px" alt="User Image">!-
+          <div>
+              <p class="app-sidebar__user-name"><b>Admin</b></p>
+              <p class="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
+          </div>
       </div>
-    </div>
-    <hr>
-    <ul class="app-menu">
+      <hr>
+      <ul class="app-menu">
 
-      <li><a class="app-menu__item active" href="."><i class='app-menu__icon bx bx-tachometer'></i><span class="app-menu__label">Bảng điều khiển</span></a></li>
-      <li><a class="app-menu__item active3" href="banner"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý banner</span></a></li>
-      <li><a class="app-menu__item active1" href="nguoidung"><i class='app-menu__icon bx bx-id-card'></i> <span class="app-menu__label">Quản lý tài khoản</span></a></li>
-      <li><a class="app-menu__item active5" href="danhmuc"><i class='app-menu__icon bx bx-run'></i><span class="app-menu__label">Quản lý danh mục</span></a></li>
-      <li><a class="app-menu__item active6" href="loaisanpham"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý loại sản phẩm</span></a></li>
-      <li><a class="app-menu__item active2" href="sanpham"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý sản phẩm</span></a></li>
-      <li><a class="app-menu__item active4" href="hoadon"><i class='app-menu__icon bx bx-task'></i><span class="app-menu__label">Quản lý đơn hàng</span></a></li>
-      <li><a class="app-menu__item active7" href="khuyenmai"><i class='app-menu__icon bx bx-pie-chart-alt-2'></i><span class="app-menu__label">Quản lý khuyến mãi</span></a> </li>
-      <li><a class="app-menu__item active7" href="khuyenmai"><i class='app-menu__icon bx bxl-blogger'></i><span class="app-menu__label">Quản lý Blogs</span></a> </li>
-      <li><a class="app-menu__item" href="../?act=taikhoan&xuli=dangxuat"><i class='app-menu__icon bx bx-cog'></i><span class="app-menu__label">Đăng xuất</span></a></li>
+          <li><a class="app-menu__item active" href="."><i class='app-menu__icon bx bx-tachometer'></i><span class="app-menu__label">Bảng điều khiển</span></a></li>
+          <li><a class="app-menu__item active3" href="banner"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý banner</span></a></li>
+          <li><a class="app-menu__item active1" href="nguoidung"><i class='app-menu__icon bx bx-id-card'></i> <span class="app-menu__label">Quản lý tài khoản</span></a></li>
+          <li><a class="app-menu__item active5" href="danhmuc"><i class='app-menu__icon bx bx-run'></i><span class="app-menu__label">Quản lý danh mục</span></a></li>
+          <!-- <li><a class="app-menu__item active6" href="loaisanpham"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý loại sản phẩm</span></a></li> -->
+          <li><a class="app-menu__item active2" href="sanpham"><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span class="app-menu__label">Quản lý sản phẩm</span></a></li>
+          <li><a class="app-menu__item active4" href="hoadon"><i class='app-menu__icon bx bx-task'></i><span class="app-menu__label">Quản lý đơn hàng</span></a></li>
+          <li><a class="app-menu__item active7" href="khuyenmai"><i class='app-menu__icon bx bx-pie-chart-alt-2'></i><span class="app-menu__label">Quản lý khuyến mãi</span></a> </li>
+          <li><a class="app-menu__item active7" href="khuyenmai"><i class='app-menu__icon bx bxl-blogger'></i><span class="app-menu__label">Quản lý Blogs</span></a> </li>
+          <li><a class="app-menu__item" href="../?act=taikhoan&xuli=dangxuat"><i class='app-menu__icon bx bx-cog'></i><span class="app-menu__label">Đăng xuất</span></a></li>
 
-    </ul>
+      </ul>
   </aside>
